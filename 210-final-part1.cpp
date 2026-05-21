36596dd6f2d76a6e8c4507ebfa82e8e7a8f86728
msVill <msv.ltmc@gmail.com>
2026-05-20 20:52:14 -0700

    update: added include guards and main only so far

---
+++ 210-final-part1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <string>
+using namespace std;
+
+
+int main(){
+    map<string, int> airportData;
+    ifstream file("210-final-1-SP26.txt");
+
+
+    return 0;
+}
