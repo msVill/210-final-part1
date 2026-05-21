aaf36574eca40b61ca1d1c0d4b4ec5b7cd737bd1
msVill <msv.ltmc@gmail.com>
2026-05-20 21:05:29 -0700

    update: added if condition for file reading, and while loop for flight data, origin,and destination

--- 210-final-part1.cpp
+++ 210-final-part1.cpp
@@ -5,10 +5,23 @@
 using namespace std;
 
 
-int main(){
+int main() {
     map<string, int> airportData;
+    
     ifstream file("210-final-1-SP26.txt");
 
+    if(!file) {
+        cout << "Error: Can't open file" << endl;
+        return 1;
+    }
+
+    string origin, destination;
+    while (file >> origin >> destination) {
+        airportData[origin]++;
+        airportData[destination]++; //What's next.. read the file.. for each flight, insert
+    }
+
+    file.close();
 
     return 0;
 }
